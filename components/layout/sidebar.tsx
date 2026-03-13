3d863dab3a342a3b287579e4558784e0be8674bb
Vicente J Lopez <lvicmdfs9@gmail.com>
2026-03-13 11:01:32 -0600

    feat: Implement a persisten state for module selected in menu side bar

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,167 +1,166 @@
-    "use client";
-    import Link from 'next/link';
-    import { usePathname } from 'next/navigation';
+"use client";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useStore } from '@/lib/store';
 
-    import {
-      CalculatorIcon,
-      PackageCheckIcon,
-      ArchiveIcon,
-      TruckIcon,
-      BrainCircuitIcon,
-      UserRoundIcon,
-      UtensilsIcon,
-      BanknoteIcon,
-      WalletIcon,
-      CornerDownLeftIcon,
-      ChartBarIcon,
-      LineChartIcon,
-      TargetIcon,
-      ClipboardListIcon,
-      CpuIcon,PiggyBankIcon,HandCoinsIcon,
-      LucideIcon,
-      BanknoteArrowDownIcon
-    } from 'lucide-react';
+import {
+  CalculatorIcon,
+  PackageCheckIcon,
+  ArchiveIcon,
+  TruckIcon,
+  BrainCircuitIcon,
+  UserRoundIcon,
+  UtensilsIcon,
+  BanknoteIcon,
+  WalletIcon,
+  CornerDownLeftIcon,
+  ChartBarIcon,
+  LineChartIcon,
+  TargetIcon,
+  ClipboardListIcon,
+  CpuIcon, PiggyBankIcon, HandCoinsIcon,
+  LucideIcon,
+  BanknoteArrowDownIcon
+} from 'lucide-react';
 
-    interface SubModule{ 
-      name: string;
-      href: string | null;
-      role: string[]; 
-      icon: LucideIcon;
-      flag?: string;
-    }
+interface SubModule {
+  name: string;
+  href: string | null;
+  role: string[];
+  icon: LucideIcon;
+  flag?: string;
+}
 
-    interface NavModule{
-      name: string;
-      href: string | null;
-      role: string[];
-      icon: LucideIcon;
-      sub?: SubModule[];
-      flag?: string;
-    }
+interface NavModule {
+  name: string;
+  href: string | null;
+  role: string[];
+  icon: LucideIcon;
+  sub?: SubModule[];
+  flag?: string;
+}
 
-    const MODULES: NavModule[] = [
-      {
-        name: 'Ventas',
-        href: '/pos',
-        role: ['ADMIN', 'STAFF'],
-        icon: CalculatorIcon 
-      },
-      {
-        name: 'Gestión',
-        href: null,
-        role: ["ADMIN"],
-        icon: ClipboardListIcon,
-        sub: [
-          { name: 'Deudores', href: '/debtors', role: ['ADMIN', 'STAFF'], icon: BanknoteIcon },
-          { name: 'Inventario', href: '/admin/inventory', role: ['ADMIN'], icon: ArchiveIcon },
-          { name: 'Proveedores', href: '/admin/suppliers', role: ['ADMIN'], icon: TruckIcon,flag: "BETA" },
-          { name: 'Menu', href: '/admin/menu', role: ['ADMIN'], icon: UtensilsIcon },
-          { name: 'CRM', href: '/admin/crm', role: ['ADMIN'], icon: UserRoundIcon },
-        ]
-      },
-      {
-        name: 'Optimiza', 
-        href: null,
-        role: ["ADMIN"],
-        icon: BrainCircuitIcon,
-        sub: [
-          { name: 'Motor de Análisis', href: '/admin/analysis/engine', role: ['ADMIN'], icon: CpuIcon }, // Interfaz para el motor
-          { name: 'Predicciones', href: '/admin/analysis/predictions', role: ['ADMIN'], icon: LineChartIcon }, // Resultados del modelo
-          { name: 'Validación', href: '/admin/analysis/accuracy', role: ['ADMIN'], icon: TargetIcon }, // Comparativa Real vs Predicción
-          { name: 'Suministro Sugerido', href: '/admin/analysis/supply', role: ['ADMIN'], icon: PackageCheckIcon }, // Aplicación práctica
-        ],
-        flag: "BETA"
-        
-      },
-      {
-        name: 'Finanzas',
-        href: null,
-        role: ["ADMIN", "STAFF"],
-        icon: WalletIcon,
-        sub: [
-          { name: 'Salarios', href: '/admin/roster', role: ['ADMIN'], icon: HandCoinsIcon },
-          { name: 'Ahorros', href: '/admin/savings', role: ['ADMIN'], icon: PiggyBankIcon, flag:"BETA" },
-          { name: 'Egresos', href: '/expenses', role: ['ADMIN', 'STAFF'], icon: BanknoteArrowDownIcon },
-          { name: 'Estadísticas', href: '/admin/statistics', role: ['ADMIN'], icon: ChartBarIcon, flag: "BETA" },
-        ]
-      }
-    ];
+const MODULES: NavModule[] = [
+  {
+    name: 'Ventas',
+    href: '/pos',
+    role: ['ADMIN', 'STAFF'],
+    icon: CalculatorIcon
+  },
+  {
+    name: 'Gestión',
+    href: null,
+    role: ["ADMIN"],
+    icon: ClipboardListIcon,
+    sub: [
+      { name: 'Deudores', href: '/debtors', role: ['ADMIN', 'STAFF'], icon: BanknoteIcon },
+      { name: 'Inventario', href: '/admin/inventory', role: ['ADMIN'], icon: ArchiveIcon },
+      { name: 'Proveedores', href: '/admin/suppliers', role: ['ADMIN'], icon: TruckIcon, flag: "BETA" },
+      { name: 'Menu', href: '/admin/menu', role: ['ADMIN'], icon: UtensilsIcon },
+      { name: 'CRM', href: '/admin/crm', role: ['ADMIN'], icon: UserRoundIcon },
+    ]
+  },
+  {
+    name: 'Optimiza',
+    href: null,
+    role: ["ADMIN"],
+    icon: BrainCircuitIcon,
+    sub: [
+      { name: 'Motor de Análisis', href: '/admin/analysis/engine', role: ['ADMIN'], icon: CpuIcon }, // Interfaz para el motor
+      { name: 'Predicciones', href: '/admin/analysis/predictions', role: ['ADMIN'], icon: LineChartIcon }, // Resultados del modelo
+      { name: 'Validación', href: '/admin/analysis/accuracy', role: ['ADMIN'], icon: TargetIcon }, // Comparativa Real vs Predicción
+      { name: 'Suministro Sugerido', href: '/admin/analysis/supply', role: ['ADMIN'], icon: PackageCheckIcon }, // Aplicación práctica
+    ],
+    flag: "BETA"
 
-    import { Button } from '../ui/button';
-    import { useState } from 'react';
+  },
+  {
+    name: 'Finanzas',
+    href: null,
+    role: ["ADMIN", "STAFF"],
+    icon: WalletIcon,
+    sub: [
+      { name: 'Salarios', href: '/admin/roster', role: ['ADMIN'], icon: HandCoinsIcon },
+      { name: 'Ahorros', href: '/admin/savings', role: ['ADMIN'], icon: PiggyBankIcon, flag: "BETA" },
+      { name: 'Egresos', href: '/expenses', role: ['ADMIN', 'STAFF'], icon: BanknoteArrowDownIcon },
+      { name: 'Estadísticas', href: '/admin/statistics', role: ['ADMIN'], icon: ChartBarIcon, flag: "BETA" },
+    ]
+  }
+];
 
-    export default function Sidebar({ userRole }: { userRole: string }) {
-      const pathname = usePathname();
-      const [subMenu, setSubMenu] = useState("");
+import { Button } from '../ui/button';
 
-      const allowedModules = MODULES.filter(m => m.role.includes(userRole));
+export default function Sidebar({ userRole }: { userRole: string }) {
+  const pathname = usePathname();
+  const subMenu = useStore((state) => state.subMenu);
+  const setSubMenu = useStore((state) => state.setSubMenu);
 
-      const activeParent = allowedModules.find(m => m.name === subMenu);
+  const allowedModules = MODULES.filter(m => m.role.includes(userRole));
 
-      return (
-        <div className='z-10 flex flex-col items-center w-25   h-screen bg-white shadow-sm font-rounded py-4'>
-          
-          {!activeParent && allowedModules.map((item, i) => {
-            const isActive = pathname === item.href;
-            
-            if (item.href) {
-              return (
-                <Link
-                  href={item.href}
-                  key={item.href}
-                  className={`flex my-1 flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer mb-4 transition-all shadow-md ${
-                    isActive ? 'bg-orange-400' : 'bg-gray-200'
-                  }`}
-                >
-                  <item.icon size={25} className='mb-1' />
-                  <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{item.name}</p>
-                </Link>
-              );
-            }
+  const activeParent = allowedModules.find(m => m.name === subMenu);
 
-            if(item.flag == "BETA") return null;
+  return (
+    <div className='z-10 flex flex-col items-center w-25   h-screen bg-white shadow-sm font-rounded py-4'>
+
+      {!activeParent && allowedModules.map((item, i) => {
+        const isActive = pathname === item.href;
+
+        if (item.href) {
+          return (
+            <Link
+              href={item.href}
+              key={item.href}
+              className={`flex my-1 flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer mb-4 transition-all shadow-md ${isActive ? 'bg-orange-400' : 'bg-gray-200'
+                }`}
+            >
+              <item.icon size={25} className='mb-1' />
+              <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{item.name}</p>
+            </Link>
+          );
+        }
+
+        if (item.flag == "BETA") return null;
+
+        return (
+          <Button
+            key={i}
+            onClick={() => setSubMenu(item.name)}
+            className="flex my-1 flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer mb-4"
+          >
+            <item.icon size={25} className='mb-1 ' />
+            <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{item.name}</p>
+          </Button>
+        );
+      })}
+
+      {activeParent && (
+        <>
+          {activeParent.sub?.filter(subItem => subItem.role.includes(userRole)).map((subItem) => {
+            const isActive = pathname === subItem.href;
+            if (subItem.flag == "BETA") return null;
 
             return (
-              <Button
-                key={i}
-                onClick={() => setSubMenu(item.name)}
-                className="flex my-1 flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer mb-4"
+              <Link
+                href={subItem.href || "#"}
+                key={subItem.name}
+                className={`text-center text-ellipsis flex my-1 flex-col items-center justify-center w-16 h-16 rounded-xl cursor-pointer mb-4 transition-all shadow-md ${isActive ? 'bg-orange-400' : 'bg-gray-200'
+                  }`}
               >
-                <item.icon size={25} className='mb-1 ' />
-                <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{item.name}</p>
-              </Button>
+                <subItem.icon size={25} className='mb-1' />
+                <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{subItem.name}</p>
+              </Link>
             );
           })}
 
-          {activeParent && (
-            <>
-              {activeParent.sub?.filter(subItem => subItem.role.includes(userRole)).map((subItem) => {
-                const isActive = pathname === subItem.href;
-              if(subItem.flag == "BETA") return null;
-              
-                return (
-                  <Link
-                    href={subItem.href || "#"}
-                    key={subItem.name}
-                    className={`text-center text-ellipsis flex my-1 flex-col items-center justify-center w-16 h-16 rounded-xl cursor-pointer mb-4 transition-all shadow-md ${
-                      isActive ? 'bg-orange-400' : 'bg-gray-200'
-                    }`}
-                  >
-                    <subItem.icon size={25} className='mb-1' />
-                    <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full '>{subItem.name}</p>
-                  </Link>
-                );
-              })}
-
-              <Button 
-                onClick={() => setSubMenu("")}
-                className="flex flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer "
-              >
-                <CornerDownLeftIcon size={25} className='mb-1 ' />
-                <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full px-1'>Volver</p>
-              </Button>
-            </>
-          )}
-        </div>
-      );
-    }
+          <Button
+            onClick={() => setSubMenu("")}
+            className="flex flex-col items-center justify-center w-18 h-18 rounded-xl cursor-pointer "
+          >
+            <CornerDownLeftIcon size={25} className='mb-1 ' />
+            <p className='text-[10px] leading-tight font-medium text-center wrap-break-word w-full px-1'>Volver</p>
+          </Button>
+        </>
+      )}
+    </div>
+  );
+}
--- lib/store.ts
+++ lib/store.ts
@@ -1,14 +1,21 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface AppState {
-  // vars
-  count: number;
+    count: number;
   userName: string;
-  // Actions
+  subMenu: string;
   increment: (qty: number) => void;
   setUserName: (name: string) => void;
+  setSubMenu: (moduleName: string) => void;
   reset: () => void;
 }
+
+interface SidebarState {
+  subMenu: string;
+  setSubMenu: (name: string) => void;
+}
+
 
 type userState = {
   id: number | null
@@ -20,19 +27,31 @@ type userAction = {
 
 export const useUserStore = create<userState & userAction>((set)=>({
     id: null,
+    subMenu: "",
+
     updateID: (newID: number) => set(()=>({id: newID}))
 }))
 
-export const useStore = create<AppState>((set) => ({
-  // Initial State
-  count: 0,
-  userName: 'Guest',
 
-  // Actions
-  increment: (qty) => set((state) => ({ count: state.count + qty })),
-  
-  setUserName: (name) => set({ userName: name }),
 
-  reset: () => set({ count: 0, userName: 'Guest' }),
-}));
 
+export const useStore = create<AppState>()(
+  persist(
+    (set) => ({
+      // Initial State
+      count: 0,
+      userName: '',
+      subMenu: "",
+
+      // Actions
+      increment: (qty) => set((state) => ({ count: state.count + qty })),
+      setUserName: (name) => set({ userName: name }),
+      setSubMenu: (moduleName) => set({ subMenu: moduleName }),
+      reset: () => set({ count: 0, userName: '', subMenu: "" }),
+    }),
+    {
+      name: 'app-storage', 
+      storage: createJSONStorage(() => localStorage), 
+    }
+  )
+);
